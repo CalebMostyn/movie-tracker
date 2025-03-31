1b2cd01c5e75260137a1e2a6ba42668fb627e308
CalebMostyn <mostyncr@miamioh.edu>
2025-03-31 13:51:18 -0400

    Additional PHP Debug Code

--- php/api.php
+++ php/api.php
@@ -1,23 +1,24 @@
 <?php
-// Get the URL from the query parameter
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $url = $_GET['url'] ?? '';
 
 if ($url) {
-    // Initialize cURL session
     $ch = curl_init();
-
-    // Set cURL options
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-    // Execute the cURL command
     $response = curl_exec($ch);
-
-    // Close cURL session
+    $curl_error = curl_error($ch);
     curl_close($ch);
 
-    // Return the response
-    echo "<html>testing testing </html>//$response;
+    if ($curl_error) {
+        echo "cURL Error: $curl_error";
+    } else {
+        echo $response;
+    }
 } else {
     echo 'URL parameter is missing.';
 }
